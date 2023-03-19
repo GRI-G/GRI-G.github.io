f3dba3bea546469d81e3f153bde9e8302e351e68
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2023-03-20 01:21:28 +0900

    Update information.ts

--- src/config/information.ts
+++ src/config/information.ts
@@ -1,3 +1,3 @@
 export const issueLink : string = "https://github.com/GRI-G/GRI-G.github.io/issues/new";
 export const repoOwner : string ="https://github.com/sunrabbit123";
-export const signinLink : string = "https://github.com/login/oauth/authorize?client_id=685ffb52e4dd768b3f66&redirect_uri=https://lxurl0clv6.execute-api.ap-northeast-2.amazonaws.com/api/auth&scope=user:email";
+export const signinLink : string = "https://github.com/login/oauth/authorize?client_id=685ffb52e4dd768b3f66&redirect_uri=https://lxurl0clv6.execute-api.ap-northeast-2.amazonaws.com/auth/auth&scope=user:email";
